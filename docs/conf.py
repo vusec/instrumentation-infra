81d5300abec81f07d904b9d4028291b59b561123
Taddeus Kroes <taddeuskroes@gmail.com>
2018-05-04 16:05:08 +0200

    Enable autodoc-annotations

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_autodoc_annotation',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
