aae26d00e59c9783d662dfa2fe4143d8e7030c12
Taddeus Kroes <taddeuskroes@gmail.com>
2018-05-07 16:25:35 +0200

    Docstrings for utils

--- infra/util.py
+++ infra/util.py
@@ -12,6 +12,20 @@ from contextlib import redirect_stdout
 
 def apply_patch(ctx, path, strip_count):
     """
+    Applies a patch in the current directory by calling ``patch -p<strip_count>
+    < <path>``.
+
+    Afterwards, a stamp file called ``.patched-<basename>`` is created to
+    indicate that the patch has been applied. If the stamp file is already
+    present, the patch is not applied at all. ``<basename>`` is generated from
+    the patch file name: ``path/to/my-patch.patch`` becomes ``my-patch``.
+
+    :param Namespace ctx: The current context.
+    :param str path: Path to the patch file.
+    :param int strip_count: Number of leading elements to strip from patch paths.
+    :returns: ``True`` if the patch was applied, ``False`` if it was already
+              applied before.
+    :rtype: bool
     """
     path = os.path.abspath(path)
     name = os.path.basename(path).replace('.patch', '')
@@ -33,6 +47,43 @@ def apply_patch(ctx, path, strip_count):
 def run(ctx, cmd, allow_error=False, silent=False, teeout=False, defer=False,
         env={}, **kwargs):
     """
+    Wrapper for :func:`subprocess.run` that does environment/output logging and
+    provides a few useful options. The log file is ``build/log/commands.txt``.
+    Where possible, use this wrapper in favor of :func:`subprocess.run` to
+    facilitate easier debugging.
+
+    It is useful to permanently have a terminal window open running ``tail -f
+    build/log/commands.txt``, This way, command output is available in case of
+    errors but does not clobber the setup's progress log.
+
+    The run environment is based on :any:`os.environ`, first adding
+    ``ctx.runenv`` (populated by packages/instances, see also
+    :class:`infra.Setup`) and then the ``env`` parameter. The combination of
+    ``ctx.runenv`` and ``env`` is logged to the log file.
+
+    If the command exits with a non-zero status code, the corresponding output
+    is logged to the command line and the process is killed with
+    ``sys.exit(-1)``.
+
+    :param Namespace ctx: The current context.
+    :param cmd: The command to run. Can be a string or a list of strings like in
+                                    :func:`subprocess.run`.
+    :type cmd: str or list[str]
+    :param bool allow_error: By default, ``sys.exit(-1)`` is called if the
+                             command returns an error. Set to True to avoid
+                             this.
+    :param bool silent: Disables output logging (only logs the envocation and
+                        environment).
+    :param bool teeout: Streams command output to ``sys.stdout`` as well as to
+                        the log file.
+    :param bool defer: Do not wait for the command to finish. Similar to
+                       ``./program &`` in Bash. Returns a
+                       :class:`subprocess.Popen` instance.
+    :param dict env: Variables to add to the environment.
+    :param kwargs: Passed directly to :func:`subprocess.run` (or
+                   :class:`subprocess.Popen` if ``defer==True``).
+    :returns: A handle to the completed or running process.
+    :rtype: subprocess.CompletedProcess or subprocess.Popen
     """
     cmd = shlex.split(cmd) if isinstance(cmd, str) else [str(c) for c in cmd]
     cmd_print = qjoin(cmd)
@@ -121,12 +172,25 @@ def run(ctx, cmd, allow_error=False, silent=False, teeout=False, defer=False,
 
 def qjoin(args):
     """
+    Join the command-line arguments to a single string to make it safe to pass
+    to paste in a shell. Basically this adds quotes to each element containing
+    spaces (uses :func:`shlex.quote`).
+
+    :param Iterable[str] args: Arguments to join.
+    :returns: The joined arguments.
+    :rtype: str
     """
     return ' '.join(shlex.quote(arg) for arg in args)
 
 
 def download(ctx, url, outfile=None):
     """
+    Download a file (logs to the debug log).
+
+    :param Namespace ctx: The current context.
+    :param str url: URL to the file to download.
+    :param outfile: Optional path/filename to download to.
+    :type outfile: str or None
     """
     if outfile:
         ctx.log.debug('downloading %s to %s' % (url, outfile))
@@ -192,6 +256,9 @@ class _Tee(io.IOBase):
 
 class Namespace(dict):
     """
+    A dictionary in which keys can be accessed as attributes, i.e., ``ns.key``
+    is the same as ``ns['key']``. Used for the context (see
+    :class:`infra.Setup`).
     """
     def __getattr__(self, key):
         return self[key]
@@ -201,6 +268,10 @@ class Namespace(dict):
 
     def copy(self):
         """
+        Make a deepcopy of this namespace, but only copy values with type
+        ``Namespace|list|dict``.
+
+        :rtype: Namespace
         """
         ns = self.__class__()
         for key, value in self.items():
@@ -211,6 +282,11 @@ class Namespace(dict):
 
     def join_paths(self):
         """
+        Create a new namespace in which all lists/tuples of strings are
+        replaced with ``':'.join(list_or_tuple)``. Used by :func:`run` to squash
+        lists of paths in environment variables.
+
+        :rtype: Namespace
         """
         new = self.__class__()
         for key, value in self.items():
@@ -224,5 +300,9 @@ class Namespace(dict):
 
 class FatalError(Exception):
     """
+    Raised for errors that should stop the execution immediately, but do not
+    need a backtrace. Results in only the exception message being logged. This
+    typically means there is an error in the user input, rather than in the code
+    that raises the error.
     """
     pass
