4d1e3596b18242ec52bf92f963d35deb0edc4160
Taddeus Kroes <taddeuskroes@gmail.com>
2018-05-14 13:27:16 +0200

    Remove some obsolete instance helpers

--- infra/instances/helpers/llvm_lto.py
+++ infra/instances/helpers/llvm_lto.py
@@ -1,18 +1,3 @@
-from . import llvm
-
-
-def configure(ctx):
-    llvm.configure(ctx)
-    path = '%s/lib/libplugins.so' % ctx.paths.prefix
-    ctx.cflags += ['-flto']
-    ctx.ldflags += ['-flto', '-Wl,-plugin-opt=-load=%s' % path]
-
-    if ctx.disable_opt:
-        ctx.cflags += ['-g3', '-O0']
-        ctx.ldflags += ['-g3', '-O0']
-        add_lto_args(ctx, '-disable-opt')
-
-
 def add_lto_args(ctx, *args):
     for arg in args:
         ctx.ldflags.append('-Wl,-plugin-opt=' + str(arg))
