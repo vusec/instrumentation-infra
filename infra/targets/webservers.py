e6714d1f9bbd0f887c2146616185cbd9d45e753d
Taddeus Kroes <taddeuskroes@gmail.com>
2019-10-07 20:45:07 +0200

    Implement --parallel parameter for nginx

--- infra/targets/webservers.py
+++ infra/targets/webservers.py
@@ -5,12 +5,22 @@ import time
 from abc import ABCMeta, abstractmethod
 from hashlib import md5
 from urllib.request import urlretrieve
+from ..packages import Bash, BenchmarkUtils
+from ..parallel import ProcessPool, PrunPool
+from ..target import Target
 from ..util import run, require_program, download, qjoin, param_attrs, \
                    FatalError
-from ..target import Target
+
 
 
 class WebServer(Target, metaclass=ABCMeta):
+    def __init__(self):
+        self.butils = BenchmarkUtils(self)
+
+    def dependencies(self):
+        yield Bash('4.3')
+        yield self.butils
+
     def add_run_args(self, parser):
         parser.add_argument('-t', '-type',
                 dest='run_type', required=True, choices=('serve', 'test', 'ab'),
@@ -36,6 +46,10 @@ class WebServer(Target, metaclass=ABCMeta):
                 help='a list of concurrency levels to run ab with (ab -c); '
                      'start low and increment until the server is saturated')
 
+    def add_report_args(self, parser):
+        self.butils.add_report_args(parser)
+        # TODO
+
     def run(self, ctx, instance, pool=None):
         runner = WebServerRunner(self, ctx, instance, pool)
 
@@ -46,81 +60,115 @@ class WebServer(Target, metaclass=ABCMeta):
         elif ctx.args.run_type == 'ab':
             runner.run_ab()
 
-    def rundir(self, ctx, instance, *args):
-        return self.path(ctx, instance.name, 'run', *args)
-
-    def rootdir(self, ctx, instance, *args):
-        return self.rundir(ctx, instance, 'www', *args)
-
     @abstractmethod
-    def start_server(self, ctx, instance):
-        """
-        Prepare the run directory and create a command to start the server.
-        """
+    def populate_rundir(self, ctx, instance):
         pass
 
     @abstractmethod
-    def stop_server(self, ctx, instance):
-        """
-        Kill the server previously started by :ref:`start_server`.
-        """
+    def start_script(self, ctx, instance):
+        pass
+
+    @abstractmethod
+    def stop_script(self, ctx, instance):
         pass
 
 
 class WebServerRunner:
     @param_attrs
     def __init__(self, server, ctx, instance, pool):
-        self.rundir = server.rundir(ctx, instance)
-        self.rootdir = server.rootdir(ctx, instance)
+        if self.pool:
+            self.rundir = self.server.butils.outfile_path(ctx, instance, 'tmp')
+        else:
+            self.rundir = self.server.path(ctx, instance.name, 'run')
+        self.rootdir = os.path.join(self.rundir, 'www')
+
+    def outfile_path(self, outfile):
+        return self.server.butils.outfile_path(self.ctx, self.instance, outfile)
 
     def run_serve(self):
         if self.pool:
-            raise FatalError('serve mode does not support parallel runs')
-        self.ctx.log.info('running web server until killed')
+            raise FatalError('can not serve interactively in parallel mode')
+
         self.create_rundir()
-        self.populate_rootdir()
         self.start_server()
+
         try:
             self.ctx.log.info('press ctrl-C to kill the server')
             while True:
                 time.sleep(100000)
         except KeyboardInterrupt:
-            self.stop_server()
-            self.remove_rundir()
+            pass
+
+        self.stop_server()
+        self.remove_rundir()
 
     def run_test(self):
-        self.ctx.log.info('running simple local test by fetching index.html')
+        self.create_rundir()
+
         if self.pool:
-            raise NotImplementedError
+            server_command = self.bash_command(self.server_script())
+            outfile = self.outfile_path('server.out')
+            self.ctx.log.debug('server will log to ' + outfile)
+            self.pool.run(self.ctx, server_command, jobid='server', nnodes=1,
+                          outfile=outfile)
+
+            client_command = self.bash_command(self.test_client_script())
+            outfile = self.outfile_path('client.out')
+            self.ctx.log.debug('client will log to ' + outfile)
+            self.pool.run(self.ctx, client_command, jobid='client', nnodes=1,
+                          outfile=outfile)
         else:
-            self.create_rundir()
-            self.populate_rootdir()
             self.start_server()
             self.request_and_check_index()
             self.stop_server()
             self.remove_rundir()
 
     def run_ab(self):
-        self.ctx.log.info('running ab test on two saparate nodes')
         if not self.pool:
             raise FatalError('need prun to run ApacheBench')
+        elif isinstance(self.pool, ProcessPool):
+            self.ctx.log.warn('ab should not run on the same machine as the '
+                              'server, use prun instead for benchmarking')
+
+        self.ctx.log.info('running ab test on two separate nodes')
         require_program('ab', 'ApacheBench is not installed')
         raise NotImplementedError
+
+    def start_server(self):
+        self.ctx.log.info('starting server')
+        script = self.server.start_script(self.ctx, self.instance)
+        run(self.ctx, self.bash_command(script), teeout=True)
+
+    def stop_server(self):
+        self.ctx.log.info('stopping server')
+        script = self.server.stop_script(self.ctx, self.instance)
+        run(self.ctx, self.bash_command(script), teeout=True)
+
+    def bash_command(self, script):
+        return ['bash', '-c', 'set -e; cd %s; %s' % (self.rundir, script)]
 
     def create_rundir(self):
         if os.path.exists(self.rundir):
             self.ctx.log.debug('removing old run directory ' + self.rundir)
             shutil.rmtree(self.rundir)
+
         self.ctx.log.debug('creating temporary run directory ' + self.rundir)
         os.makedirs(self.rundir)
         os.chdir(self.rundir)
 
+        self.ctx.log.debug('populating run directory')
+        self.server.populate_rundir(self.ctx, self.instance)
+        self.populate_rootdir()
+
     def remove_rundir(self):
+        assert not self.pool
         self.ctx.log.debug('removing run directory ' + self.rundir)
         shutil.rmtree(self.rundir)
 
     def populate_rootdir(self):
         require_program(self.ctx, 'dd', 'required to generate index.html')
+        self.ctx.log.info('creating index.html of size %s with random '
+                          'contents' % self.ctx.args.filesize)
         os.makedirs(self.rootdir, exist_ok=True)
         run(self.ctx, ['dd', 'if=/dev/urandom', 'bs=1',
                        'of=%s/index.html' % self.rootdir,
@@ -139,13 +187,50 @@ class WebServerRunner:
         if got != expected:
             self.stop_server()
             raise FatalError('content does not match generated index.html')
-        self.ctx.log.info('content of index.html is correct')
+        self.ctx.log.info('contents of index.html are correct')
 
-    def start_server(self):
-        self.server.start_server(self.ctx, self.instance)
+    def server_script(self):
+        start_script = self.server.start_script(self.ctx, self.instance)
+        stop_script = self.server.stop_script(self.ctx, self.instance)
 
-    def stop_server(self):
-        self.server.stop_server(self.ctx, self.instance)
+        if isinstance(self.pool, PrunPool):
+            domain_command = 'hostname'
+        else:
+            domain_command = 'echo localhost'
+
+        return '''
+        set -x
+        {domain_command} > domain
+        {start_script}
+
+        echo "waiting for another process to create stop_server file"
+        while [ ! -e stop_server ]; do sleep 0.2; done
+
+        echo "stopping server"
+        {stop_script}
+        '''.format(**locals())
+
+    def test_client_script(self):
+        domain = 'localhost'
+        port = self.ctx.args.port
+
+
+        return '''
+        while [ ! -e {self.rundir}/nginx.pid ]; do sleep 0.1; done
+        url="http://$(cat domain):{port}/index.html"
+        echo "requesting $url"
+        wget -q -O requested_index.html "$url"
+
+        echo "creating stop_server to stop web server"
+        touch stop_server
+
+        if diff -q requested_index.html {self.rootdir}/index.html; then
+            echo "contents of index.html are correct"
+        else
+            echo "ERROR: fetched content does not match generated index.html"
+            exit 1
+        fi
+        '''.format(**locals())
 
 
 class Nginx(WebServer):
@@ -157,7 +242,6 @@ class Nginx(WebServer):
     """
 
     name = 'nginx'
-    config_file = 'config.conf'
 
     #: :class:`list` Command line arguments for the built-in ``-allocs`` pass;
     #: Registers custom allocation function wrappers in Nginx.
@@ -168,8 +252,9 @@ class Nginx(WebServer):
         'ngx_palloc_large' ':malloc' ':1',
     ))]
 
+    @param_attrs
     def __init__(self, version):
-        self.version = version
+        super().__init__()
 
     def fetch(self, ctx):
         download(ctx, 'https://nginx.org/download/' + self.tar_name())
@@ -180,7 +265,7 @@ class Nginx(WebServer):
     def tar_name(self):
         return 'nginx-' + self.version + '.tar.gz'
 
-    def build(self, ctx, instance, pool=None):
+    def build(self, ctx, instance):
         if not os.path.exists(instance.name):
             require_program(ctx, 'tar', 'required to unpack source tarfile')
             ident = 'nginx-' + self.version
@@ -194,7 +279,6 @@ class Nginx(WebServer):
         if self.should_configure(ctx):
             ctx.log.debug('no Makefile or flags changed, reconfiguring')
             run(ctx, ['./configure',
-                      '--prefix=' + self.rundir(ctx, instance),
                       '--with-cc=' + ctx.cc,
                       '--with-cc-opt=' + qjoin(ctx.cflags),
                       '--with-ld-opt=' + qjoin(ctx.ldflags)])
@@ -236,14 +320,11 @@ class Nginx(WebServer):
         parser.add_argument('--workers', type=int, default=1,
                 help='number of worker processes')
 
-    def start_server(self, ctx, instance):
-        # write config file
+    def populate_rundir(self, ctx, instance):
         port = ctx.args.port
-        rundir = self.rundir(ctx, instance)
-        rootdir = self.rootdir(ctx, instance)
-        ctx.log.debug('writing config to ' + self.config_file)
-        config_template = """
-error_log {rundir}/error.log;
+        rundir = os.getcwd()
+        ctx.log.debug('writing config to nginx.conf')
+        config_template = '''
 lock_file {rundir}/nginx.lock;
 pid {rundir}/nginx.pid;
 worker_processes {ctx.args.workers};
@@ -253,23 +334,29 @@ http {{
         listen {port};
         server_name localhost;
         location / {{
-        root {rootdir};
+        root {rundir}/www;
         }}
     }}
 }}
-"""
-        with open(self.config_file, 'w') as f:
+'''
+        with open('nginx.conf', 'w') as f:
             f.write(config_template.format(**locals()))
 
-        # start server
+        # need logs/ directory for error+access logs
         os.mkdir('logs')
-        ctx.log.debug('starting server on port %d' % port)
-        run(ctx, ['../objs/nginx', '-c', self.config_file], teeout=True)
-        with open('nginx.pid') as f:
-            self.pid = int(f.read())
-        ctx.log.info('started server on port %d, pid %d' % (port, self.pid))
 
-    def stop_server(self, ctx, instance):
-        ctx.log.info('stopping server')
-        run(ctx, ['../objs/nginx', '-s', 'quit', '-c', self.config_file],
-            teeout=True)
+    def start_script(self, ctx, instance):
+        rundir = os.getcwd()
+        objdir = self.path(ctx, instance.name, 'objs')
+        return '''
+        {objdir}/nginx -p {rundir} -c nginx.conf
+        echo "started server on port {ctx.args.port}, pid $(cat nginx.pid)"
+        '''.format(**locals())
+
+    def stop_script(self, ctx, instance):
+        rundir = os.getcwd()
+        objdir = self.path(ctx, instance.name, 'objs')
+        #return objdir + '/nginx -c nginx.conf -s quit'
+        return '''
+        {objdir}/nginx -p {rundir} -c nginx.conf -s quit
+        '''.format(**locals())
