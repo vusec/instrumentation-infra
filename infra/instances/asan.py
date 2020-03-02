1b651773ca91ba0e42cc6f9686c6cc009952d472
Taddeus Kroes <taddeuskroes@gmail.com>
2020-03-02 18:46:44 +0100

    Add configuration options to ASan instance for temporal safety and disabling checks

--- infra/instances/asan.py
+++ infra/instances/asan.py
@@ -18,19 +18,25 @@ class ASan(Clang):
     :name: asan[-heap|-nostack|-noglob][-wo][-lto]
     :param llvm: an LLVM package with compiler-rt included
     :param stack: toggle stack instrumentation
+    :param temporal: toggle temporal safety (`False` sets quarantine size to 0)
     :param glob: toggle globals instrumentation
-    :param only_writes: only instrument writes
+    :param check_writes: toggle checks on stores
+    :param check_reads: toggle checks on loads
     :param lto: perform link-time optimizations
     """
     @param_attrs
-    def __init__(self, llvm: LLVM, *, stack=True, glob=True,
-                 only_writes=False, lto=False):
+    def __init__(self, llvm: LLVM, *, temporal=True, stack=True, glob=True,
+                 check_writes=True, check_reads=True, lto=False):
         assert llvm.compiler_rt, 'ASan needs LLVM with runtime support'
         super().__init__(llvm, lto=lto)
+        assert not check_reads or check_writes, 'will not check reads without writes'
 
     @property
     def name(self):
         name = 'asan'
+
+        if not self.temporal:
+            name += '-spatial'
 
         if not self.stack and not self.glob:
             name += '-heap'
@@ -39,8 +45,8 @@ class ASan(Clang):
         elif not self.glob:
             name += '-noglob'
 
-        if self.only_writes:
-            name += '-wo'
+        if not self.check_reads:
+            name += '-wo' if self.check_writes else '-nochecks'
 
         if self.lto:
             name += '-lto'
@@ -54,8 +60,11 @@ class ASan(Clang):
             cflags = ['-mllvm', '-asan-stack=0']
         if not self.glob:
             cflags += ['-mllvm', '-asan-globals=0']
-        if self.only_writes:
+        if not self.check_reads:
             cflags += ['-mllvm', '-asan-instrument-reads=false']
+        if not self.check_writes:
+            cflags += ['-mllvm', '-asan-instrument-writes=false']
+            cflags += ['-mllvm', '-asan-instrument-atomics=false']
         ctx.cflags += cflags
         ctx.cxxflags += cflags
         ctx.ldflags += ['-fsanitize=address']
@@ -65,11 +74,14 @@ class ASan(Clang):
             'alloc_dealloc_mismatch': 0,
             'detect_odr_violation': 0,
             'detect_leaks': 0,
-            # uncomment the following to disable quarantining, thus disabling
-            # temporal safety:
-            # TODO: make this a configuration option
-            #'detect_stack_use_after_return': 0,
-            #'thread_local_quarantine_size_kb': 0,
-            #'quarantine_size_mb': 0
         }
+
+        if not self.temporal:
+            opts['detect_stack_use_after_return'] = 0
+            opts['thread_local_quarantine_size_kb'] = 0
+            opts['quarantine_size_mb'] = 0
+
+        if not self.check_writes:
+            opts['replace_intrin'] = 0
+
         ctx.runenv.ASAN_OPTIONS = ':'.join('%s=%s' % i for i in opts.items())
